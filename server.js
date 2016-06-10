df060c7988cb22144d4f3ab02a8069966e399522
Scott <scottgangemi@gmail.com>
2016-06-10 17:18:42 +1000

    Created modules to generate links, with some particularly interesting names like 'yellow-mellow-incisor', should be fun.

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-var express=require('express');
-var app=express();
-var path=require('path');
-var mongo=require('mongodb').MongoClient;
+var express = require('express');
+var path = require('path');
+var mongo = require('mongodb').MongoClient;
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var wordList = require('word-list-json');
 
-var port=process.env.PORT || 8080;
+var app = express();
+var submit = require('./submit');
+
+var port = process.env.PORT || 8080;
 app.listen(port);
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+submit.parseColors();
 
-app.get('/', function (req, res) {
-	//so, this will serve up a HTML page that invites a user to type in the url they want to shorten.
+app.use(bodyParser.json());
+app.post("/", function(req, res) {
+    //console.log(req.body.url);
+    //submit.test(req.body.url);
+    submit.prepareLink();
+    //do something with url.
 });
 
-app.get('/:shortened', function(req, res) {
-	input=req.params.shortened;
-	//this will take the users input (which is a short link), and then poll the database to see if we have the link, if we don't it'll return an error. if we do, then it'll redirect the user to the link that's stored at that point in the database.
-});
+
+// //Here we are configuring express to use body-parser as middle-ware.
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 //inport node modules in here.
 
 //need to initialise database as well. not sure if that should be here, or elsewhere. we can dig, all good.
+//module to get a user's input that gets submitted. won't be too hard, maybe should just do first???
+//module to pick random words, then write to db, then fed back to user.
+//module to check db for link, act accordingly. 
+//will need a HTML page to show the new URL. shouldn't need a 'new' page, jsut the old one with DOM manipulation.
